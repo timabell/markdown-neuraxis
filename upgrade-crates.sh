b3b6d0f589aa0dbc13c24d8ce1cce4f012366e69
Tim Abell <t@0x5.uk>
2025-09-14 22:28:18 +0100

    Update upgrade script to cope with deeper cargo.toml files

--- upgrade-crates.sh
+++ upgrade-crates.sh
@@ -4,4 +4,4 @@ cargo update
 cargo install cargo-edit
 cargo upgrade --incompatible # from cargo-edit
 cargo test
-git commit -i Cargo.lock -i Cargo.toml -m "chore: dependency upgrades"
+git commit -i Cargo.lock -i Cargo.toml -i **/Cargo.toml -m "chore: dependency upgrades"
