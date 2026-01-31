a0d84626ba791df070e893396556fb2f07c349c5
Tim Abell <t@0x5.uk>
2026-01-31 23:15:45 +0000

    Disable emulator gpu
    Misbehaving over x2go
    
    https://developer.android.com/studio/run/emulator-acceleration

--- run-emulator.sh
+++ run-emulator.sh
@@ -5,4 +5,4 @@ set -e
 source set-android-envs.sh
 
 # run list-emulator-devices.sh to get valid device names
-emulator -avd Medium_Phone_API_36.1 -netdelay none -netspeed full
+emulator -avd Medium_Phone_API_36.1 -netdelay none -netspeed full -gpu swiftshader_indirect
