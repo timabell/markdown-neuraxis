f402aa56ad23dba90c3140ccc5e35f5026a43602
Tim Abell <t@0x5.uk>
2026-02-01 13:41:43 +0000

    Add headless android sh
    For running on another machine connecting scrcpy over sshy

---
+++ run-emulator-headless.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -v
+# https://dioxuslabs.com/learn/0.7/guides/platforms/mobile#android
+set -e
+
+source set-android-envs.sh
+
+# run list-emulator-devices.sh to get valid device names
+emulator -avd Medium_Phone_API_36.1 -netdelay none -netspeed full -no-window
