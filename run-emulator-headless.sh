fbc25fbf86c0b8163adf756c76177611e5397676
Tim Abell <t@0x5.uk>
2026-03-24 22:20:32 +0000

    Disable gpu for headless android
    Was getting a load of errors like this and a mostly blank display over x2go
    
    /buildbot/src/googleplex-android/emu-36-3-release/hardware/google/gfxstream/host/gl/glestranslator/gles_v2/gles_v2_imp.cpp:glUniformMatrix3fv:4036 error 0x502

--- run-emulator-headless.sh
+++ run-emulator-headless.sh
@@ -5,7 +5,7 @@ set -e
 source set-android-envs.sh
 
 # run list-emulator-devices.sh to get valid device names
-emulator -avd Medium_Phone_API_36.1 -netdelay none -netspeed full -no-window
+emulator -avd Medium_Phone_API_36.1 -netdelay none -netspeed full -no-window -gpu swiftshader_indirect
 
 # Connect to gui with scrcpy - https://github.com/Genymobile/scrcpy
 # ssh -N -L 5554:localhost:5554 -L 5555:localhost:5555 <host> # Forward ports
